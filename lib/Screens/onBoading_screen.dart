6319b7af8ef19349c1d3f8115827c7c79705324f
soumya12062013 <soumya.dec2006@gmail.com>
2025-10-25 13:30:44 +0530

    adde picture and text in onboading screen

---
+++ lib/Screens/onBoading_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class OnBoadingScreen extends StatelessWidget {
+  const OnBoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 238, 233, 220),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset("Assets/Images/Mask group.png"),
+              SizedBox(height: 50),
+              SizedBox(
+                child: Center(
+                  child: Text(
+                    'Stay Focused',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.1,
+                      fontSize: 28,
+                      color: const Color.fromARGB(255, 78, 53, 44),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                child: Center(
+                  child: Text(
+                    'Get the cup filled of your choice to stay',
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 0.25,
+                      fontWeight: FontWeight.w300,
+                      color: const Color.fromARGB(255, 165, 140, 131),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                child: Center(
+                  child: Text(
+                    'focused and awake .Different type of coffee ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 0.25,
+                      fontWeight: FontWeight.w300,
+                      color: const Color.fromARGB(255, 165, 140, 131),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                child: Center(
+                  child: Text(
+                    'menu, hot lotte cappuchino',
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 0.25,
+                      fontWeight: FontWeight.w300,
+                      color: const Color.fromARGB(255, 165, 140, 131),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
