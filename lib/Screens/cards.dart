cb92f6dbf0324452e4b7bf8666eaec7adb655d22
soumya12062013 <soumya.dec2006@gmail.com>
2025-10-28 09:25:00 +0530

    created the price and plus icon in the cunstom card

---
+++ lib/Screens/cards.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class coffeecards extends StatefulWidget {
+  String imagepath;
+  String title;
+  String Subtitle;
+  String price;
+  String Rating;
+  coffeecards({
+    super.key,
+    required this.imagepath,
+    required this.title,
+    required this.Subtitle,
+    required this.Rating,
+    required this.price,
+  });
+
+  @override
+  State<coffeecards> createState() => _coffeecardsState();
+}
+
+class _coffeecardsState extends State<coffeecards> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.white10,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(widget.imagepath, fit: BoxFit.cover),
+                  ),
+                ),
+                Positioned(
+                  top: 10,
+                  left: 10,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 5,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.4),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.star, color: Colors.amber, size: 18),
+                        SizedBox(width: 5),
+                        Text(
+                          widget.Rating,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Text(
+                widget.title,
+                style: TextStyle(
+                  fontSize: 20,
+                  letterSpacing: 1.1,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Text(widget.Subtitle, style: TextStyle(fontSize: 14)),
+            ),
+            Spacer(),
+            Padding(
+              padding: EdgeInsetsGeometry.all(15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.price,
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFC67C4E),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Icon(Icons.add, color: Colors.white, size: 24),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
